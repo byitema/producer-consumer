1205ed6e40ff8048294d1382f00d8bdd9247c186
Artem Bulakh <artem.bulakh1@gmail.com>
2021-04-05 01:40:11 +0300

    solved

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import random
 import string
+import threading
+import queue
+import time
 
 
 def randstr() -> str:
@@ -13,16 +16,52 @@ def randstr() -> str:
     return result
 
 
-def count_unique(string: str):
+def count_unique(string: str) -> int:
     counts = dict()
     for symbol in string:
         counts[symbol] = counts.get(symbol, 0) + 1
     
-    return len(counts)
+    return (string, len(counts))
     
 
+def producer(q: queue.Queue, cond: threading.Condition, n: int):
+    while True:
+        time.sleep(n)
+        cond.acquire()
+        try:
+            q.put(randstr())
+            print("String produced by Producer with id: " + str(threading.get_ident()))
+            cond.notify()
+        finally:
+            cond.release()
+
+
+def consumer(q: queue.Queue, cond: threading.Condition):
+    cond.acquire()
+    while True:
+        try:
+            tmp = count_unique(q.get_nowait())
+            print("Answer: " + str(tmp[1]) + " for string: " + tmp[0])
+        except:
+            value = cond.wait()
+            if value:
+                print("Notification for Consumer with id: " + str(threading.get_ident()))
+                continue
+            else:
+                print("Timeout")
+                break
+
+    cond.release()
+
+
 if __name__ == '__main__':
-    for i in range(10):
-        tmp = randstr()
-        print(tmp)
-        print(count_unique(tmp))
+    q = queue.Queue()
+    cond = threading.Condition()
+
+    threading.Thread(target=producer, args=(q,cond, 2,)).start()
+    threading.Thread(target=producer, args=(q,cond, 3,)).start()
+    threading.Thread(target=producer, args=(q,cond, 5,)).start()
+    threading.Thread(target=producer, args=(q,cond, 6,)).start()
+
+    threading.Thread(target=consumer, args=(q,cond,)).start()
+    threading.Thread(target=consumer, args=(q,cond,)).start()
